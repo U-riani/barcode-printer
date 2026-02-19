2e4fc69fdd9a764ce85e6cf892cde3b25c19fa81
U-riani <sandropapiashvili@gmail.com>
2026-02-19 19:07:50 +0400

    s

--- src/components/BarcodeCell.jsx
+++ src/components/BarcodeCell.jsx
@@ -11,7 +11,7 @@ export default function BarcodeCell({ value }) {
       <Barcode
         value={clean}
         format="CODE128"
-        width={1.1}
+        width={0.8}
         height={20}
         fontSize={10}
         displayValue={true}
