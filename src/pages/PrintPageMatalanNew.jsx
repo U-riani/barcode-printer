3a3423a44fde4072c68c674e8eedbbfe337b8349
U-riani <sandropapiashvili@gmail.com>
2026-02-19 16:47:46 +0400

    s

--- src/pages/PrintPageMatalanNew.jsx
+++ src/pages/PrintPageMatalanNew.jsx
@@ -99,7 +99,7 @@ export default function PrintPageMatalanNew() {
         const pageEl = pagesEls[i];
 
         // force exact size before capture
-        pageEl.style.width = "260mm";
+        pageEl.style.width = "280mm";
         pageEl.style.height = "297mm";
 
         // scale: window.devicePixelRatio || 2,
